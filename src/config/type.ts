3d90a7a1bb885d61f02d9ddebd9b48265fad8222
mckrava <mckrava2@gmail.com>
2021-05-25 22:51:47 +0200

    Add Fee type

--- src/config/type.ts
+++ src/config/type.ts
@@ -1,42 +1,43 @@
 export default {
-  "AssetPair": {
-    "asset_in": "AssetId",
-    "asset_out": "AssetId"
+  AssetPair: {
+    asset_in: 'AssetId',
+    asset_out: 'AssetId',
   },
-  "Amount": "i128",
-  "AmountOf": "Amount",
-  "Address": "AccountId",
-  "OrmlAccountData": {
-    "free": "Balance",
-    "frozen": "Balance",
-    "reserved": "Balance"
+  Amount: 'i128',
+  AmountOf: 'Amount',
+  Address: 'AccountId',
+  OrmlAccountData: {
+    free: 'Balance',
+    frozen: 'Balance',
+    reserved: 'Balance',
   },
-  "BalanceInfo": {
-    "amount": "Balance",
-    "assetId": "AssetId"
+  BalanceInfo: {
+    amount: 'Balance',
+    assetId: 'AssetId',
   },
-  "Chain": {
-    "genesisHash": "Vec<u8>",
-    "lastBlockHash": "Vec<u8>"
+  Chain: {
+    genesisHash: 'Vec<u8>',
+    lastBlockHash: 'Vec<u8>',
   },
-  "CurrencyId": "AssetId",
-  "CurrencyIdOf": "AssetId",
-  "Intention": {
-    "who": "AccountId",
-    "asset_sell": "AssetId",
-    "asset_buy": "AssetId",
-    "amount": "Balance",
-    "discount": "bool",
-    "sell_or_buy": "IntentionType"
+  CurrencyId: 'AssetId',
+  CurrencyIdOf: 'AssetId',
+  Intention: {
+    who: 'AccountId',
+    asset_sell: 'AssetId',
+    asset_buy: 'AssetId',
+    amount: 'Balance',
+    discount: 'bool',
+    sell_or_buy: 'IntentionType',
   },
-  "IntentionId": "Hash",
-  "IntentionType": {
-    "_enum": [
-      "SELL",
-      "BUY"
-    ]
+  IntentionId: 'Hash',
+  IntentionType: {
+    _enum: ['SELL', 'BUY'],
   },
-  "LookupSource": "AccountId",
-  "OrderedSet": "Vec<AssetId>",
-  "Price": "Balance"
-}
+  LookupSource: 'AccountId',
+  OrderedSet: 'Vec<AssetId>',
+  Price: 'Balance',
+  Fee: {
+    numerator: 'u32',
+    denominator: 'u32',
+  },
+};
