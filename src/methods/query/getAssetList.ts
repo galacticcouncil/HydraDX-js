27eb90f5e1625e3a96e03eb8bfbc350166637f20
mckrava <mckrava2@gmail.com>
2021-10-14 11:46:29 +0200

    Add blockHash support for method getAssetList

--- src/methods/query/getAssetList.ts
+++ src/methods/query/getAssetList.ts
@@ -1,25 +1,27 @@
 import { AssetRecord } from '../../types';
 import Api from '../../api';
 
-export async function getAssetList() {
+export async function getAssetList(blockHash?: string | undefined) {
   return new Promise(async (resolve, reject) => {
     try {
       const api = Api.getApi();
       if (!api) return resolve([]);
-      const assetIds = await api.query.assetRegistry.assetIds.entries();
+      const assetIds = blockHash
+        ? await api.query.assetRegistry.assetIds.entriesAt(blockHash)
+        : await api.query.assetRegistry.assetIds.entries();
       const assetList: AssetRecord[] = [{ assetId: 0, name: 'HDX' }];
-    
+
       // TODO: Better way to parse mapped records
       assetIds.forEach(([assetName, id]) => {
         const assetId = parseInt(api.createType('Option<u32>', id).toString());
         const name = assetName.toHuman()?.toString() || '0xERR';
-    
+
         assetList[assetId] = { assetId, name };
       });
-    
+
       resolve(assetList);
-    } catch(e: any) {
+    } catch (e: any) {
       reject(e);
     }
   });
-};
+}
